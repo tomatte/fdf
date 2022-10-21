70b3038c12c0c16323abc63d27decf705786de3e
tomatte <daniel.brandao2000@gmail.com>
2022-10-21 16:57:42 -0300

    alternative pixel put

---
+++ my_mlx_pixel_put.c
@@ -0,0 +1,9 @@
+#include "fdf.h"
+
+void	my_mlx_pixel_put(t_data *img, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = img->addr + (y * img->size_line + x * (img->bpp / 8));
+	*(unsigned int*)dst = color;
+}
