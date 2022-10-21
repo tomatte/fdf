054812193db9bbd7d7e9dce1f6702730e334686c
tomatte <daniel.brandao2000@gmail.com>
2022-10-21 18:25:44 -0300

    tests

--- fdf.c
+++ fdf.c
@@ -15,6 +15,17 @@ void	init_fill(t_data *img, int width, int height, char title[])
 	img->window = mlx_new_window(img->mlx, width, height, title);
 }
 
+void	draw_line(t_data *img, int x, int y, int x2, int y2)
+{
+	t_line	line_data;
+
+	line_data.x = x;
+	line_data.y = y;
+	line_data.x2 = x2;
+	line_data.y2 = y2;
+	line(img, &line_data);
+}
+
 int main(void)
 {
 	t_data	img;
@@ -22,39 +33,41 @@ int main(void)
 	ft_printf("Lib working!\n");
 	init_fill(&img, 1080, 720, "Land");
 	new_img(img.mlx, &img);
-/* 	line(&img, 1080, 0, 0, 720);
-	line(&img, 0, 0, 1080, 720);
-	line(&img, 1080/2, 0, 1080/2, 720);
-	line(&img, 0, 360, 1080, 360); */
+/* 	draw_line(&img, 1080, 0, 0, 720);
+	draw_line(&img, 0, 0, 1080, 720);
+	draw_line(&img, 1080/2, 0, 1080/2, 720);
+	draw_line(&img, 0, 360, 1080, 360); */
 
-	line(&img, 1080/2, 720/2, 1, 1);
-	line(&img, 1080/2, 720/2, 1080/2, 1);
-	line(&img, 1080/2, 720/2, 1, 720);
-	line(&img, 1080/2, 720/2, 1, 720);
-	line(&img, 1080/2, 720/2, 1080, 720);
-	line(&img, 1080/2, 720/2, 1080, 1);
-	line(&img, 1080/2, 720/2, 1080/4*3, 1);
-	line(&img, 1080/2, 720/2, 1080/4, 1);
+/* 	//-----------------start----------------
+	draw_line(&img, 1080/2, 720/2, 1, 1);
+	draw_line(&img, 1080/2, 720/2, 1080/2, 1);
+	draw_line(&img, 1080/2, 720/2, 1, 720);
+	draw_line(&img, 1080/2, 720/2, 1, 720);
+	draw_line(&img, 1080/2, 720/2, 1080, 720);
+	draw_line(&img, 1080/2, 720/2, 1080, 1);
+	draw_line(&img, 1080/2, 720/2, 1080/4*3, 1);
+	draw_line(&img, 1080/2, 720/2, 1080/4, 1);
 
 	//left triangle
-	line(&img, 1080/2 - 200, 720/2, 1080/2 - 200 - 100, 720/2 - 70);
-	line(&img, 1080/2 - 200, 720/2, 1080/2 - 200 - 100, 720/2 + 70);
-	line(&img, 1080/2 - 300, 720/2 + 70, 1080/2 - 300, 720/2 - 70);
+	draw_line(&img, 1080/2 - 200, 720/2, 1080/2 - 200 - 100, 720/2 - 70);
+	draw_line(&img, 1080/2 - 200, 720/2, 1080/2 - 200 - 100, 720/2 + 70);
+	draw_line(&img, 1080/2 - 300, 720/2 + 70, 1080/2 - 300, 720/2 - 70);
 
 	//right triangle
-	line(&img, 1080/2 + 200, 720/2, 1080/2 + 300, 720/2 + 70);
-	line(&img, 1080/2 + 200, 720/2, 1080/2 + 300, 720/2 - 70);
-	line(&img, 1080/2 + 300, 720/2 - 70, 1080/2 + 300, 720/2 + 70);
+	draw_line(&img, 1080/2 + 200, 720/2, 1080/2 + 300, 720/2 + 70);
+	draw_line(&img, 1080/2 + 200, 720/2, 1080/2 + 300, 720/2 - 70);
+	draw_line(&img, 1080/2 + 300, 720/2 - 70, 1080/2 + 300, 720/2 + 70);
 
-	line(&img, 1080/2 - 100, 720/2 + 200, 1080/2 + 100, 720/2 + 200);
+	draw_line(&img, 1080/2 - 100, 720/2 + 200, 1080/2 + 100, 720/2 + 200);
+	//-----------------end---------------- */
 
-/* 	line(&img, 540, 0, 540, 360);
-	line(&img, 0, 0, 540, 360);
-	line(&img, 540, 360, 1080, 0); */
-	//line(&img, 540, 0, 540, 360);
+/* 	draw_line(&img, 540, 0, 540, 360);
+	draw_line(&img, 0, 0, 540, 360);
+	draw_line(&img, 540, 360, 1080, 0); */
+	//draw_line(&img, 540, 0, 540, 360);
 
-/* 	line(&img, 0, 0, 50, 15);
-	line(&img, 0, 0, 50, 15); */
+/* 	draw_line(&img, 0, 0, 50, 15);
+	draw_line(&img, 0, 0, 50, 15); */
 
 	mlx_put_image_to_window(img.mlx, img.window, img.img, 0, 0);
 	mlx_loop(img.mlx);
