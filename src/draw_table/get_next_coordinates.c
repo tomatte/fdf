8eb8d8f8df072b8fbe0de64d87d59da03496b250
tomatte <daniel.brandao2000@gmail.com>
2022-10-26 16:54:21 -0300

    find_number() ok

--- src/draw_table/get_next_coordinates.c
+++ src/draw_table/get_next_coordinates.c
@@ -1,21 +1,62 @@
 #include "../fdf.h"
 
+int find_number(char **str)
+{
+    int num;
+
+    while (**str == ' ' || **str == '\t')
+        (*str)++;
+    if (ft_isdigit(**str))
+    {
+        num = ft_atoi(*str);
+        while (ft_isdigit(**str))
+            (*str)++;
+        return (num);
+    }
+    return (-1);
+}
+/* 
 int get_line_size(char *map_line)
 {
+    int i;
+
     if (!map_line)
         return (0);
-    while ()
+    i = 0;
+    while (find_number(&map_line) > 0)
+        i++;
+    return (i);
 }
 
 int *get_next_coordinates(int fd)
 {
-    char    *map_line;
-    int     line_size;
-    int     *coordinates;
+    char	*map_line;
+    int		line_size;
+    int		*coordinates;
 
     map_line = get_next_line(fd);
     line_size = get_line_size(map_line);
     coordinates = alloc_int_array(line_size);
+} */
 
-    /* 0 0 20 0 20 0 0 0 20 */
+int main(void)
+{
+	// TESTS //
+
+	//--- find_number() ---//
+    int num;
+	char	*str;
+
+	ft_printf("find_number() tests:\n");
+	str = ft_strdup(" 89 100 ");
+    num = find_number(&str);
+	ft_printf("res == 89:\t%d\n", num);
+    num = find_number(&str);
+	ft_printf("res == 100:\t%d\n", num);
+	str = ft_strdup("  t2 -10");
+    num = find_number(&str);
+	ft_printf("res == -1:\t%d\n", num);
+	num = find_number(&str);
+	ft_printf("res == -1:\t%d\n", num);
+	ft_printf("-------------------\n");
 }
