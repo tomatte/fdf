bc59d10545458bb7f12177db06b3bc8cfa1d0513
tomatte <daniel.brandao2000@gmail.com>
2022-10-26 17:15:32 -0300

    get_line_size() ok

--- src/draw_table/get_next_coordinates.c
+++ src/draw_table/get_next_coordinates.c
@@ -15,7 +15,7 @@ int find_number(char **str)
     }
     return (-1);
 }
-/* 
+
 int get_line_size(char *map_line)
 {
     int i;
@@ -23,9 +23,21 @@ int get_line_size(char *map_line)
     if (!map_line)
         return (0);
     i = 0;
-    while (find_number(&map_line) > 0)
+    while (find_number(&map_line) >= 0)
         i++;
     return (i);
+}
+
+int	*get_int_array(int line_size)
+{
+	int	*coordinates;
+	int	i;
+
+	coordinates = ft_calloc(line_size + 1, sizeof(int));
+	if (!coordinates)
+		return (NULL);
+	coordinates[0] = line_size + 1;
+	return (coordinates);
 }
 
 int *get_next_coordinates(int fd)
@@ -36,8 +48,8 @@ int *get_next_coordinates(int fd)
 
     map_line = get_next_line(fd);
     line_size = get_line_size(map_line);
-    coordinates = alloc_int_array(line_size);
-} */
+    coordinates = get_int_array(line_size);
+}
 
 int main(void)
 {
@@ -47,7 +59,7 @@ int main(void)
     int num;
 	char	*str;
 
-	ft_printf("find_number() tests:\n");
+	ft_printf("FIND_NUMBER FUNCTION TEST:\n");
 	str = ft_strdup(" 89 100 ");
     num = find_number(&str);
 	ft_printf("res == 89:\t%d\n", num);
@@ -59,4 +71,18 @@ int main(void)
 	num = find_number(&str);
 	ft_printf("res == -1:\t%d\n", num);
 	ft_printf("-------------------\n");
+	//--------------------------------//
+
+	int	size;
+	ft_printf("GET_LINE_SIZE FUNCTION TEST:\n");
+	size = get_line_size(" 0 0 10 10 20 10 0 0");
+	ft_printf("res == 8:\t%d\n", size);
+	size = get_line_size(" 50 50 50 0");
+	ft_printf("res == 4:\t%d\n", size);
+	size = get_line_size("50 50 0");
+	ft_printf("res == 3:\t%d\n", size);
+	size = get_line_size("0 0");
+	ft_printf("res == 2:\t%d\n", size);
+	size = get_line_size("t0 20");
+	ft_printf("res == 0:\t%d\n", size);
 }
