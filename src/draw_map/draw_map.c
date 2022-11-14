64684d3ad49ece7320dbf18072fc93515db844d0
tomatte <daniel.brandao2000@gmail.com>
2022-11-13 23:18:43 -0300

    in development

--- src/draw_map/draw_map.c
+++ src/draw_map/draw_map.c
@@ -1,8 +1,32 @@
 #include "../fdf.h"
 
-static void	draw_between_numbers(int lines, int columns, char *map)
+static void	draw_between_columns(t_data *img, char *map, int i, int j)
 {
-	
+	//get item i, j  and i, ++j;
+	draw_isometric_line();
+}
+
+static void	draw_between_lines(t_data *img, char *map, int i, int j)
+{
+	//get item i, j and ++i, j;
+	draw_isometric_line();
+}
+
+static void	draw(t_data *img, int lines, int columns, char *map)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (++i < lines)
+	{
+		j = 0;
+		while (++j < columns)
+		{
+			draw_between_columns(img, map, i, j);
+		}
+		draw_between_lines(img, map, i, j);
+	}
 }
 
 void	draw_map(t_data *img, char *map)
@@ -11,8 +35,6 @@ void	draw_map(t_data *img, char *map)
 	int	columns;
 
 	lines = get_map_lines(map);
-	ft_printf("lines: %d\n", lines);
 	columns = get_map_columns(map);
-	ft_printf("columns: %d\n", columns);
-	draw_between_numbers(lines, columns, map);
+	draw(img, lines, columns, map);
 }
