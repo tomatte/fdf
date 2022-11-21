51957f93206b9d44f2a8c08ba17593ead5186ca0
tomatte <daniel.brandao2000@gmail.com>
2022-11-20 21:05:01 -0300

    now closing with red x too

--- src/hooks/close_window.c
+++ src/hooks/close_window.c
@@ -1,13 +1,23 @@
 #include "../fdf.h"
 
-static int	close_w(int key_code, t_img *img)
+static int	close_esc(int key_code, t_img *img)
 {
 	if (key_code == ESC)
 		mlx_destroy_window(img->mlx, img->window);
+	ft_printf("\nWindow closed\n");
+	return (0);
+}
+
+static int	close_x(t_img *img)
+{
+	mlx_destroy_window(img->mlx, img->window);
+	ft_printf("\nProgram closed\n");
+	exit(0);
 	return (0);
 }
 
 void	close_window(t_img *img)
 {
-	mlx_key_hook(img->window, close_w, img);
+	mlx_key_hook(img->window, close_esc, img);
+	mlx_hook(img->window, 17, 1L<<17,  close_x, img);
 }
