2afbf9c4ab0231c0cd7b6b6457831b93408165fb
tomatte <daniel.brandao2000@gmail.com>
2022-11-28 00:05:04 -0300

    extracting values from map and storing on a different type of linked list

---
+++ src/get_map/dot_functions.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   dot_functions.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/27 18:51:06 by dbrandao          #+#    #+#             */
+/*   Updated: 2022/11/27 23:58:56 by dbrandao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../fdf.h"
+
+static int	is_new_line(char **map)
+{
+	while (ft_isspace(**map))
+	{
+		if (**map == '\n')
+			return (1);
+		else
+			(*map)++;
+	}
+	return (0);
+}
+
+static int	extract_color(char **map)
+{
+	int	color;
+
+	if (**map == ',')
+		(*map)++;
+	else
+		return (DEFAULT_COLOR);
+	color = ft_hextoi_mod(map);
+	return (color);
+}
+
+void	*new_dot(char **map)
+{
+	t_dot	*dot;
+
+	if (!map || !*map || !**map)
+		return (NULL);
+	dot = (t_dot *) malloc(sizeof(t_dot));
+	if (!dot)
+		return (NULL);
+	dot->depth = ft_atoi_mod(map);
+	dot->color = extract_color(map);
+	dot->front = NULL;
+	dot->down = NULL;
+	return (dot);
+}
+
+void	get_first(t_dot **dots, t_dot **first, t_dot **current, t_dot **up, char **map)
+{
+	if (!*dots)
+	{
+		*dots = new_dot(map);
+		*first = *dots;
+		*current = *first;
+	}
+	else if (is_new_line(map))
+	{
+		*up = *first;
+		*current = new_dot(map);
+		*first = *current;
+		(*up)->down = *current;
+	}
+}
+
+void	get_next(t_dot **current, t_dot **up, char **map)
+{
+	(*current)->front = new_dot(map);
+	*current = (*current)->front;
+	if (*up && (*up)->front && *current)
+	{
+		(*up) = (*up)->front;
+		(*up)->down = *current;
+	}
+}
---
+++ src/get_map/get_map_values.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_map_values.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/26 14:04:58 by dbrandao          #+#    #+#             */
+/*   Updated: 2022/11/28 00:04:11 by dbrandao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../fdf.h"
+
+t_dot	*get_map_values(char *map)
+{
+	t_dot	*dots;
+	t_dot	*up;
+	t_dot	*current;
+	t_dot	*first;
+
+	if (!map)
+		return (NULL);
+	up = NULL;
+	first = NULL;
+	current = NULL;
+	dots = NULL;
+	while (*map)
+	{
+		get_first(&dots, &first, &current, &up, &map);
+		get_next(&current, &up, &map);
+		if (!current)
+			break ;
+	}
+	return (dots);
+}
+
+/* 
+int	main(int argc, char **argv)
+{
+	t_dot	*dot;
+	t_dot	*line;
+	char	*map;
+
+	map = get_map(argv[1]);
+	dot = get_map_values(map);
+	line = dot;
+
+	while (dot)
+	{
+		ft_printf("%d:%d\t", dot->depth, dot->color);
+		dot = (t_dot *) dot->front;
+		if (!dot)
+		{
+			ft_printf("\n\n\n");
+			dot = (t_dot *) line->down;
+			line = dot;
+		}
+	}
+	return (0);
+}
+ */
+//gcc map_struct.c read_file.c ../others/error_exit.c ../others/mem_clear.c ./get_map.c ../../libft/libftprintf.a
+
+
+
+/* 
+	var a;
+	var up;
+
+	up = NULL;
+	up = a;
+	up->down = b
+	up = up->front
+	up->down = b2
+	up = up->front
+	up->down = b3;
+	a - a2 - a3
+	|	|  - |
+	b - b2 - b3
+	|
+	c - c2 - c3
+
+	up = up->down;
+	up->down = c;
+
+	c->front = c2;
+
+	up = up->next
+	up->down = c2;
+
+
+
+
+ */
