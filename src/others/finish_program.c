f301a77f67ae72a3beaf07ad3ad8c2dd4d243d88
tomatte <daniel.brandao2000@gmail.com>
2022-12-07 09:17:55 -0300

    memory leaks fixed

--- src/others/finish_program.c
+++ src/others/finish_program.c
@@ -6,7 +6,7 @@
 /*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 23:14:59 by dbrandao          #+#    #+#             */
-/*   Updated: 2022/12/07 00:08:41 by dbrandao         ###   ########.fr       */
+/*   Updated: 2022/12/07 09:17:25 by dbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,23 @@
 static void	free_dots(t_dot *dots)
 {
 	t_dot		*aux;
-	t_dot		*first;
+	t_dot		*down;
 
-
-	first = dots;
-	aux = NULL;
+	aux = (t_dot *) dots->front;
+	down = (t_dot *) dots->down;
 	while (dots)
 	{
-		if (dots->front)
-			aux = (t_dot *) dots->front;
-		else
-			aux = NULL;
-		free(dots);
-		dots = NULL;
+		mem_clear((void *) &dots);
 		if (aux)
-			dots = aux;
-		if (!dots && first->down)
 		{
-			dots = (t_dot *) first->down;
-			first = dots;
+			dots = aux;
+			aux = (t_dot *) dots->front;
+		}
+		else if (down)
+		{
+			dots = down;
+			aux = (t_dot *) dots->front;
+			down = (t_dot *) dots->down;
 		}
 	}
 }
@@ -45,5 +43,6 @@ void	finish_program(t_img *img)
 	mlx_destroy_display(img->mlx);
 	free_dots(img->dots);
 	free(img->map);
+	free(img->mlx);
 	exit(0);
 }
